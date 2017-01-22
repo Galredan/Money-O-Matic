7b5c4dad30af44088645a7965b82555eda74dcc2
Julien <julien.fournier@ynov.com>
2017-01-22 10:12:16 +0100

    Ajout des filles et des animations

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,50 +1,73 @@
-function machineASous () {
-	var creditJoueur = document.getElementById("credit").innerHTML;
-	var highscore = document.getElementById("highscore").innerHTML;
-	var modal = document.getElementById('myModal');
-	var gain = 0;
-	var i = 0;
-	var a;
-	while (i<5) {
+function machineASous() {
+    var creditJoueur = document.getElementById("credit").innerHTML;
+    var highscore = document.getElementById("highscore").innerHTML;
+    var modal = document.getElementById('myModal');
+    var gain = 0;
+    var i = 0;
 
-		setTimeout('randomImg(a,img_0)',1000);
-		i++;
+    var a = Math.floor(Math.random() * 3);
+    var b = Math.floor(Math.random() * 3);
+    var c = Math.floor(Math.random() * 3);
+    var d = Math.floor(Math.random() * 3);
 
-	}
+    document.getElementById('img_0').src = 'img_' + a + '.jpg';
+    document.getElementById('img_1').src = 'img_' + b + '.jpg';
+    document.getElementById('img_2').src = 'img_' + c + '.jpg';
+    document.getElementById('img_3').src = 'img_' + d + '.jpg';
 
+    creditJoueur -= 1;
 
-	creditJoueur -= 1 ; 
+    // SI 4 IDENTIQUE GAIN 100
+    if (a == b && b == c && c == d) {
+        gain = 100;
+        creditJoueur += 100;
+        jackpot();
 
-
-	// SI 4 IDENTIQUE
-	if (a==b && b==c && c==d){
-		gain = 100;
-		creditJoueur +=100 ;
-		loadFrame();
-	}
-	// SI
-	else if (a==b ^ a==c ^ a==d ^ b==c ^ b==d ^ c==d ) {
-		gain = 15;
-		creditJoueur += 15 ;
-	}
-	if (creditJoueur>highscore){
-		highscore = creditJoueur;
-	}
-	
-	//console.log(gain);
-
-	document.getElementById("credit").innerHTML = creditJoueur;
-	document.getElementById("highscore").innerHTML = highscore;
-	document.getElementById("gain").innerHTML = gain;
-}
-function randomImg(position,positionId){
-	position = Math.floor(Math.random()*3);
-	document.getElementById(positionId).src ='img_' + position +'.jpg';
-}
-function loadFrame() {
-	var $iframe = $( '<div id="popup"><center><iframe id="popup" sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src="https://matias.ma/nsfw/" width="50%" height="50%" frameborder="0"></iframe></center></div>' );
-	$( "body" ).append($iframe);
-	setTimeout(function() {
-  		$('#popup').remove();
-	}, 10000);
+        loadFrame();
     }
+    // SI 3 IDENTIQUE SUIVIS GAIN 10
+    else if ((a == b && b == c) || (b == c && c == d)) {
+        gain = 10;
+        creditJoueur += 10;
+        petitGain();
+    }
+
+    // SI 4 DIFFERENT
+    else if ((a != b) && (b = !c) && (c != d) && (a != c) && (a != d) && (b != d)) {
+        gain = 1;
+        creditJoueur += 1;
+    }
+    if (creditJoueur > highscore) {
+        highscore = creditJoueur;
+    }
+
+    //console.log(gain);
+
+    document.getElementById("credit").innerHTML = creditJoueur;
+    document.getElementById("highscore").innerHTML = highscore;
+    document.getElementById("gain").innerHTML = gain;
+}
+
+function loadFrame() {
+    var $iframe = $('<div id="popup"><center><iframe id="popup" sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src="https://matias.ma/nsfw/" width="50%" height="50%" frameborder="0"></iframe></center></div>');
+    $("body").append($iframe);
+    setTimeout(function() {
+        $('#popup').remove();
+    }, 10000);
+}
+
+function jackpot() {
+    $('#girl1').addClass('animated pulse');
+    $('#girl2').addClass('animated pulse');
+    $('#jackpot').removeClass('bounceOutUp hidden').addClass('bounceInDown');
+    setTimeout("$('#girl1').removeClass('animated pulse');", 1500);
+    setTimeout("$('#girl2').removeClass('animated pulse');", 1500);
+    setTimeout("$('#jackpot').removeClass('bounceInDown').addClass('bounceOutUp')", 10000);
+}
+
+function petitGain() {
+    $('#girl1').addClass('animated shake');
+    $('#girl2').addClass('animated shake');
+    setTimeout("$('#girl1').removeClass('animated shake');", 1500);
+    setTimeout("$('#girl2').removeClass('animated shake');", 1500);
+}
